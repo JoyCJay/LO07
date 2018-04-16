e2d3f02bc51c99825b26a72d603715fce3e9220f
joycjay <joycjay@163.com>
2018-04-16 18:56:18 +0200

    median 2017

---
+++ Median/2017/test.php
@@ -0,0 +1,16 @@
+<?php
+//http://localhost/lo07_tp/Median/2017/test.php
+    $a = array(
+        "a"=>"a1",
+        "b"=>"b2",
+        "c"=>"c3"
+    );
+    echo $a["a"];
+    print ("<br/>");
+    echo count($a);
+    $multiple="";
+    if($multiple){
+        echo "OK";
+    }
+    
+?>
